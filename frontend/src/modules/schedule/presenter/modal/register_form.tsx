173d2fab6392df9195e698ec0a3f1827490045de
William Fabre <fabrwil@gmail.com>
2023-06-23 20:05:54 -0300

    WIP: adding newst data

--- frontend/src/modules/schedule/presenter/modal/register_form.tsx
+++ frontend/src/modules/schedule/presenter/modal/register_form.tsx
@@ -1,54 +1,17 @@
 import React from 'react';
-import {
-  Flex,
-  Box,
-  FormControl,
-  FormLabel,
-  Input,
-  Checkbox,
-  Stack,
-  Link,
-  Button,
-  Heading,
-  Text,
-  useColorModeValue,
-} from '@chakra-ui/react';
+import { Flex, Box, FormControl, FormLabel, Input, Stack } from '@chakra-ui/react';
 
 const RegisterForm: React.FC = () => {
   return (
-    <Flex minH={'100vh'} align={'center'} justify={'center'} bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'}>Sign in to your account</Heading>
-          <Text fontSize={'lg'} color={'gray.600'}>
-            to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
-          </Text>
-        </Stack>
-        <Box rounded={'lg'} bg={useColorModeValue('white', 'gray.700')} boxShadow={'lg'} p={8}>
+    <Flex minH={'60vh'} width={'100%'}>
+      <Stack py={3} px={0}>
+        <Box p={0}>
           <Stack spacing={4}>
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" />
+            <FormControl id="schedule_date">
+              <FormLabel>Data do Agendamento</FormLabel>
+              <Input placeholder="Selecione uma data" size="md" type="datetime-local" />
             </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input type="password" />
-            </FormControl>
-            <Stack spacing={10}>
-              <Stack direction={{ base: 'column', sm: 'row' }} align={'start'} justify={'space-between'}>
-                <Checkbox>Remember me</Checkbox>
-                <Link color={'blue.400'}>Forgot password?</Link>
-              </Stack>
-              <Button
-                bg={'blue.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-                }}
-              >
-                Sign in
-              </Button>
-            </Stack>
+            <FormControl id="doctor"></FormControl>
           </Stack>
         </Box>
       </Stack>
