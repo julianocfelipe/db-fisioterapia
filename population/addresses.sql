4dc4eb28be4a8723a5d3fc4755088431bda1147c
hebromjunior <72686237+hebromjunior@users.noreply.github.com>
2023-06-26 15:18:25 -0300

    WIP: INSERTS cancellation_reasons e correções

--- population/addresses.sql
+++ population/addresses.sql
@@ -1,10 +1,22 @@
-INSERT INTO addresses (cep, state, city, neighbourhood, number, complement, reference) VALUES ('96419266', 'RS', 'Bagé', 'Habitar Brasil', '1432', 'Ap 403', 'Prédio azul')
-INSERT INTO addresses (cep, state, city, neighbourhood, number, complement, reference) VALUES ('55640103', 'PE', 'GVila Real', 'Mieira', '221', '', 'Casa verde' )
-INSERT INTO addresses (cep, state, city, neighbourhood, number, complement, reference) VALUES ('15054616', 'SP', 'São Joaquim','São José', '543', 'Sobrado', 'Casa azul' )
-INSERT INTO addresses (cep, state, city, neighbourhood, number, complement, reference) VALUES ('60745050', 'RO', 'Porto Velho', 'Artur Lundgren', '2345', 'Travesa', '' )
-INSERT INTO addresses (cep, state, city, neighbourhood, number, complement, reference) VALUES ('29138065', 'ES', 'Viana', 'Cassino', '623', 'Ap 802 B', 'Na frente dos Correios' )
-INSERT INTO addresses (cep, state, city, neighbourhood, number, complement, reference) VALUES ('84020710', 'AC', 'Rio Branco', 'Piranga', '324', '', 'Na frente ao bar do Zeca' )
-INSERT INTO addresses (cep, state, city, neighbourhood, number, complement, reference) VALUES ('29178038', 'ES', 'Serra', 'Jardim', '522', 'Fundos', 'Casa cinza' )
-INSERT INTO addresses (cep, state, city, neighbourhood, number, complement, reference) VALUES ('69036100', 'AM', 'Manaus', 'Ponta Negra', '5623', 'Ap 201', 'Prédio verde e branco' )
-INSERT INTO addresses (cep, state, city, neighbourhood, number, complement, reference) VALUES ('57044137', 'AL', 'Maceió', 'São Jorge', '253', '', 'Casa amarela' )
-INSERT INTO addresses (cep, state, city, neighbourhood, number, complement, reference) VALUES ('68902130', 'AP', 'MAcapá', 'Arruda', '734', 'Ap 303', '' )
+INSERT INTO addresses (cep, state, city, neighbourhood, number, complement, reference) VALUES ('88820000', 'SC', 'Içara',    'Habitar Brasil', '1432', 'Ap 403',   'Prédio azul')
+INSERT INTO addresses (cep, state, city, neighbourhood, number, complement, reference) VALUES ('88820000', 'SC', 'Içara',    'Mieira',         '221',  '',         'Casa verde' )
+INSERT INTO addresses (cep, state, city, neighbourhood, number, complement, reference) VALUES ('88820000', 'SC', 'Içara',    'São José',       '543',  'Sobrado',  'Casa azul' )
+INSERT INTO addresses (cep, state, city, neighbourhood, number, complement, reference) VALUES ('88801006', 'SC', 'Criciúma', 'Artur Lundgren', '2345', 'Travesa',  '' )
+INSERT INTO addresses (cep, state, city, neighbourhood, number, complement, reference) VALUES ('88820000', 'SC', 'Içara',    'Cassino',        '623',  'Ap 802 B', 'Na frente dos Correios' )
+INSERT INTO addresses (cep, state, city, neighbourhood, number, complement, reference) VALUES ('88801003', 'SC', 'Criciúma', 'Piranga',        '324',  '',         'Na frente ao bar do Zeca' )
+INSERT INTO addresses (cep, state, city, neighbourhood, number, complement, reference) VALUES ('88820000', 'SC', 'Içara',    'Jardim',         '522',  'Fundos',   'Casa cinza' )
+INSERT INTO addresses (cep, state, city, neighbourhood, number, complement, reference) VALUES ('88801008', 'SC', 'Criciúma', 'Ponta Negra',    '5623', 'Ap 201',   'Prédio verde e branco' )
+INSERT INTO addresses (cep, state, city, neighbourhood, number, complement, reference) VALUES ('88820000', 'SC', 'Içara',    'São Jorge',      '253',  '',         'Casa amarela' )
+INSERT INTO addresses (cep, state, city, neighbourhood, number, complement, reference) VALUES ('88801007', 'SC', 'Içara',    'Arruda',         '734',  'Ap 303',   '' )
+
+
+UPDATE addresses SET cep = '88820000' state = 'SC' city = 'Içara'    WHERE id = 
+UPDATE addresses SET cep = '88820000' state = 'SC' city = 'Içara'    WHERE id = 
+UPDATE addresses SET cep = '88820000' state = 'SC' city = 'Içara'    WHERE id = 
+UPDATE addresses SET cep = '88801006' state = 'SC' city = 'Criciúma' WHERE id = 
+UPDATE addresses SET cep = '88820000' state = 'SC' city = 'Içara'    WHERE id = 
+UPDATE addresses SET cep = '88801003' state = 'SC' city = 'Criciúma' WHERE id = 
+UPDATE addresses SET cep = '88820000' state = 'SC' city = 'Içara'    WHERE id = 
+UPDATE addresses SET cep = '88801008' state = 'SC' city = 'Criciúma' WHERE id = 
+UPDATE addresses SET cep = '88820000' state = 'SC' city = 'Içara'    WHERE id =     
+UPDATE addresses SET cep = '88801007' state = 'SC' city = 'Içara'    WHERE id =   
---
+++ population/cancellation_reasons.sql
@@ -0,0 +1,12 @@
+--INCOPLETA!!!!!!--INCOPLETA!!!!!!--INCOPLETA!!!!!!--INCOPLETA!!!!!!--INCOPLETA!!!!!!--INCOPLETA!!!!!!--INCOPLETA!!!!!!
+INSERT INTO CANCELLATION_REASONS (description, schedules_id) VALUES ("Emergência médica do paciente",       1)
+INSERT INTO CANCELLATION_REASONS (description, schedules_id) VALUES ("Conflitos de agenda",                 1)
+INSERT INTO CANCELLATION_REASONS (description, schedules_id) VALUES ("Doença repentina",                    1)
+INSERT INTO CANCELLATION_REASONS (description, schedules_id) VALUES ("Condições climáticas adversas",       1)
+INSERT INTO CANCELLATION_REASONS (description, schedules_id) VALUES ("Falta de transporte",                 1)
+INSERT INTO CANCELLATION_REASONS (description, schedules_id) VALUES ("Mudança de residência",               1)
+INSERT INTO CANCELLATION_REASONS (description, schedules_id) VALUES ("Condição médica piorando",            1)
+INSERT INTO CANCELLATION_REASONS (description, schedules_id) VALUES ("Problemas financeiros",               1)
+INSERT INTO CANCELLATION_REASONS (description, schedules_id) VALUES ("Falta de confiança no profissional",  1)
+INSERT INTO CANCELLATION_REASONS (description, schedules_id) VALUES ("Circunstâncias pessoais imprevistas", 1) 
+--INCOPLETA!!!!!!--INCOPLETA!!!!!!--INCOPLETA!!!!!!--INCOPLETA!!!!!!--INCOPLETA!!!!!!--INCOPLETA!!!!!!--INCOPLETA!!!!!!
