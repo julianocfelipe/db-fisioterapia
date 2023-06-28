80d635df186d5f3e69d6c6fb44ba889e16e64453
CharlesClezar <103696544+CharlesClezar@users.noreply.github.com>
2023-06-27 23:19:03 -0300

    Update and rename TRIGGER_start_v3.sql to TRIGGER_EM_ANDAMENTO.sql
    Quando houver inserção na start_schedule vai mudar o schedule_status_id para 7 (em andamento)

--- TRIGGER_EM_ANDAMENTO.sql
+++ TRIGGER_EM_ANDAMENTO.sql
@@ -1,11 +1,12 @@
-CREATE TRIGGER alterScheduleStatusStart ON schedules
+/*Muda o status_status_id para 7 (Em andamento)*/
+CREATE TRIGGER alterScheduleStatusEmAndamento ON schedules
 AFTER INSERT
 AS
 BEGIN
     IF (SELECT inserted.start_schedule FROM inserted) IS NOT NULL
     BEGIN
         UPDATE schedules
-        SET schedules_status_id = 2
+        SET schedules_status_id = 7
         WHERE id = (SELECT inserted.start_schedule FROM inserted);
     END;
 END;
