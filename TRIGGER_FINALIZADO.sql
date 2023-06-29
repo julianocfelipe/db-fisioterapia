8f2f6bb8fb269a7165894d1bb346cd324724a655
CharlesClezar <103696544+CharlesClezar@users.noreply.github.com>
2023-06-29 13:03:46 -0300

    Update TRIGGER_FINALIZADO.sql
    mudança de coluna de end_schedule para end_service

--- TRIGGER_FINALIZADO.sql
+++ TRIGGER_FINALIZADO.sql
@@ -3,10 +3,10 @@ CREATE TRIGGER alterScheduleStatusFinalizado ON schedules
 AFTER INSERT
 AS
 BEGIN
-    IF (SELECT inserted.end_schedules FROM inserted) IS NOT NULL
+    IF (SELECT inserted.end_service FROM inserted) IS NOT NULL
     BEGIN
         UPDATE schedules
         SET schedules_status_id = 4
-        WHERE id = (SELECT inserted.end_schedules FROM inserted);
+        WHERE id = (SELECT inserted.end_service FROM inserted);
     END;
 END;
