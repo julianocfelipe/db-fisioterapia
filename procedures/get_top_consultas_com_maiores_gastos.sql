f0293140e4e5ff6e3b72c85dcd9c85580d1b6084
Lucas Borges Borba <69261686+lucasborba111@users.noreply.github.com>
2023-06-29 19:21:18 -0300

    Update get_top_consultas_com_maiores_gastos.sql

--- procedures/get_top_consultas_com_maiores_gastos.sql
+++ procedures/get_top_consultas_com_maiores_gastos.sql
@@ -1,7 +1,7 @@
 CREATE PROCEDURE get_top_consultas_com_maiores_gastos
 	@schedule_start_at DATETIME = NULL,
 	@schedule_end_at  DATETIME = NULL,
-	@top INT
+	@top INT = 1
 AS
 BEGIN
 	SELECT TOP (@top) schedules.id, services.price, services.description
@@ -9,12 +9,10 @@ BEGIN
 			INNER JOIN schedules ON services.id = schedules.schedules_services_id
 	WHERE (@schedule_start_at IS NULL OR schedules.start_service >= @schedule_start_at)
 	AND (@schedule_end_at IS NULL OR schedules.end_service <= @schedule_end_at)
-	AND schedules.id in (
+	AND schedules.id  in (
 		SELECT payments.schedules_id
 			FROM payments 
-		WHERE payments_status_id = 10
+		WHERE payments_status_id IN (11, 2, 10)
 	) 
 	ORDER BY services.price DESC
 END
-
-exec get_top_consultas_com_maiores_gastos @schedule_start_at = null, @schedule_end_at = null, @top = 5
