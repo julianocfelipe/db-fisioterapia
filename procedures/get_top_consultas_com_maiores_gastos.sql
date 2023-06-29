9ee64fc74b6baf4b791907a59c666e14bf886f49
lucasborba111 <lucasborgesborba@gmail.com>
2023-06-28 23:13:30 -0300

    Consulta de registros com preço em ordem decrescente e com status finalizado

--- procedures/get_top_consultas_com_maiores_gastos.sql
+++ procedures/get_top_consultas_com_maiores_gastos.sql
@@ -1,15 +1,21 @@
 CREATE PROCEDURE get_top_consultas_com_maiores_gastos
-	@start_at DATETIME = NULL,
-	@end_at  DATETIME = NULL,
+	@schedule_start_at DATETIME = NULL,
+	@schedule_end_at  DATETIME = NULL,
 	@top INT
 AS
 BEGIN
-	SELECT TOP (@top) schedules.id, dbo.schedules.body, payments.value
-	FROM payments
-	INNER JOIN schedules ON payments.schedules_id = schedules.id
-	WHERE (@start_at IS NULL OR schedules.start_service >= @start_at)
-		AND (@end_at IS NULL OR schedules.end_service <= @end_at)
-	ORDER BY payments.value DESC
+	SELECT TOP (@top) schedules.id, services.price, services.description
+		FROM services
+			INNER JOIN schedules ON services.id = schedules.schedules_services_id
+	WHERE (@schedule_start_at IS NULL OR schedules.start_service >= @schedule_start_at)
+	AND (@schedule_end_at IS NULL OR schedules.end_service <= @schedule_end_at)
+	AND schedules.id in (
+		SELECT payments.schedules_id
+			FROM payments 
+				INNER JOIN payments_status on payments.payments_status_id = payments_status.id 
+		WHERE payments_status.description = 'Finalizado'
+	) 
+	ORDER BY services.price DESC
 END
 
-exec get_top_consultas_com_maiores_gastos @start_at = null, @end_at = null, @top = 5
+exec get_top_consultas_com_maiores_gastos @schedule_start_at = null, @schedule_end_at = null, @top = 5
